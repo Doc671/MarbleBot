f29f65080cd9c568b4a22145109210382722f425
Doc671 <36280447+Doc671@users.noreply.github.com>
2021-03-03 10:17:34 +0000

    Morale boost now correctly decreases damage once it wears off

--- src/Common/Games/Siege/Siege.cs
+++ src/Common/Games/Siege/Siege.cs
@@ -23,7 +23,7 @@ namespace MarbleBot.Common.Games.Siege
             get => _activeMoraleBoosts;
             set
             {
-                DamageMultiplier *= (int)Math.Pow(2, value - _activeMoraleBoosts);
+                DamageMultiplier = (int)Math.Round(DamageMultiplier * Math.Pow(2, value - _activeMoraleBoosts));
                 _activeMoraleBoosts = value;
             }
         }
